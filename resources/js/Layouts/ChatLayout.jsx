3d88c98bce172a81ae0a5b46407b6610e36a1ede
zhuyeeng <zhuyeeng0524@gmail.com>
2024-05-18 12:49:24 +0800

    update on Chat Layout (1.12.51)

---
+++ resources/js/Layouts/ChatLayout.jsx
@@ -0,0 +1,22 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { usePage } from "@inertiajs/react";
+
+const ChatLayout = ({ children }) => {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const selectedConversations = page.props.selectedConversations;
+
+    console.log("conversations", conversations);
+    console.log("conversations", selectedConversations);
+
+    return (
+        <AuthenticatedLayout>
+            ChatLayout
+            <div>
+                [children]
+            </div>
+        </AuthenticatedLayout>
+    )
+}
+
+export default ChatLayout;
